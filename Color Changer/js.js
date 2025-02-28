9422be98a003c2eea550030fef042d2274194255
miran <miran123125@gmail.com>
2025-02-28 14:16:27 +0100

    Added a new function and got rid of old ones

--- Color Changer/js.js
+++ Color Changer/js.js
@@ -1,11 +1,3 @@
-function blue() {
-    document.getElementById("text").style.color = "blue"
-}
-
-function red() {
-    document.getElementById("text").style.color = "red"
-}
-
-function reset() {
-    document.getElementById("text").style.color = "black" 
+function setTextColor(color) {
+  document.getElementById("text").style.color = color
 }
